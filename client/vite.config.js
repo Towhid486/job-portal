549abe1d6a5671e53534c7c906a1e7a8667cf9a8
Towhid486 <developertowhid@gmail.com>
2025-03-27 09:05:07 +0600

    14th commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,13 +9,4 @@ export default defineConfig({
     tailwindcss(),
 
   ],
-  server:{
-    proxy:{
-      '/api/v1':{
-        target:"https://job-portal-server-five-mu.vercel.app",
-        secure:false,
-        changeOrigin: true,
-      }
-    }
-  }
 })
